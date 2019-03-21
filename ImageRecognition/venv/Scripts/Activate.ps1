c8a291b078a404f959b04ea51efa7e4a42f29d21
Benjamin Herger <39559055+BenjaminHerger@users.noreply.github.com>
2019-03-21 12:46:42 +0100

    Added Shielddetection

--- ImageRecognition/venv/Scripts/Activate.ps1
+++ ImageRecognition/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\GitHSLU\PREN\ImageRecognition\venv"
+$env:VIRTUAL_ENV="E:\GitHSLU\PREN\ImageRecognition\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- ImageRecognition/venv/Scripts/pip3-script.py
+++ ImageRecognition/venv/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!D:\GitHSLU\PREN\ImageRecognition\venv\Scripts\python.exe
+#!E:\GitHSLU\PREN\ImageRecognition\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip3'
 __requires__ = 'pip==10.0.1'
 import re
